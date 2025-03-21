5a02db01cbd47138cf1467b1365aa2055a00580d
Aditya2476 <aditya.gunjal9821@gmail.com>
2025-03-21 16:56:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//alert("hellolo") // we are using node js not browser 
+
+console.log (3+3)
+console.log("adi") //code readability is main priority
+
+let name = "aditya"      //string
+let age = 18            //number
+let isLoggedin = true    //booleann
+
+
+//number --> int 
+//bigint --> not used mostly
+//string ---> "" or ''
+//boolean --> true or false
+//null --> standalone value
+//undefined --> value is not assigned
+//symbol --> used for uniqueness
+
+
+//object
+
+console.log(typeof "aditya")
+console.log(typeof null)  //object
+console.log(typeof undefined) 
